30a88b0e16bdf4c3f7ada9549654761d60358bcf
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2023-11-22 12:09:39 +0000

    class has ben added

---
+++ python /functions/class.py
@@ -0,0 +1,9 @@
+class studs:
+    name = "nagendra anna"
+    occupation = "sns shop owner"
+    age = 28 
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+
+a = studs()
+a.info()
