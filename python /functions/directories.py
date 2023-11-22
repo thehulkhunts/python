788303964d466aa9c7181b4aeacd862402c0bc27
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2023-11-22 12:53:17 +0000

    modified directory files

--- python /functions/directories.py
+++ python /functions/directories.py
@@ -10,8 +10,14 @@ class aws:
     "instance_name" : "EKS",
      "instance_type" : "t2.xlarge",
      "region" : "ap-south-1"
-   }     
+   },
+   {
+    "instance_name" : "Docker",
+     "instance_type" : "t3.xlarge",
+     "region" : "ap-south-1",
+     "zone"  :  "ap-south-1a"
+   }    
 ]
 
 a = aws()
-print(a.names[0]["instance_type"])
+print(a.names[0]["instance_type"], a.names[2]["zone"])
