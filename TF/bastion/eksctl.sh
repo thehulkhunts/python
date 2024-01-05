c6adcc62e6f9ecc758b67c45201380a49ec5bbf5
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2024-01-05 15:50:18 +0000

    ADDED bastion host for EKS

---
+++ TF/bastion/eksctl.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.17/2023-11-14/bin/linux/arm64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin 
+kubectl version --client
---
+++ TF/bastion/helm.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+ curl curl -L https://git.io/get_helm.sh | bash -s -- --version v3.8.2
+ 
