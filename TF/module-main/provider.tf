531bb5bdde82c3c12a35a36644fea3c986e81bab
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2024-01-09 14:24:30 +0530

    modified some files for updation (#7)
    * modified some files for updation
    
    * Update provider.tf
    
    Added some comments to backend block

--- TF/module-main/provider.tf
+++ TF/module-main/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region  = "ap-south-1"
-  shared_credentials_files = ["/root/.aws/credentials"]
+  region                   = "ap-south-1"
+  profile                  =  "vinay" 
 }
 
 terraform {
@@ -10,5 +10,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=5.31.0"
     }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "viin-ultimate-bucket"  // name of your bucket choice
+    key = "EKS/terraform.tfstate"   // key match to the path
+    region = "us-east-1"  //region
+    dynamodb_table = "vin-ultimate-db" // name of your dynamodb table 
   }
 }
